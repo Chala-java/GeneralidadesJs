5e43769b8d8beb99ebf1bed65d15fe5b613f39e4
Chala-java <chalajuandavid876@gmail.com>
2024-10-17 16:29:14 -0500

    Se crean funciones

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let usuario = "admin";
 let password = "1234";
-let inputContraseña = prompt("Ingrese su Contraseña");
-
 let inputUsuario = prompt("Ingrese su Usuario");
 if (usuario == inputUsuario) {
   inputContraseña = prompt("Ingrese su contraseña");
@@ -10,4 +8,17 @@ if (usuario == inputUsuario) {
   } else console.log("Contraseña incorrecta");
 } else {
   console.log("Usuario no existe en la base de datos ");
+}
+
+function consultarSaldo(){
+    console.log("Su saldo es")
+}
+function retirarDinero(){
+    console.log("Hizo un retiro por")
+}
+function transferirDinero(){
+    console.log("Hizo una transferencia por")
+}
+function consignarDinero(){
+    console.log("Hizo una consignación por")
 }
