fcf3ef02f3c0989674bfbe4c8ccaa9d79c015def
Chala-java <chalajuandavid876@gmail.com>
2024-10-17 16:13:56 -0500

    Se crea y valida que funciona el inicio de sesion

--- script.js
+++ script.js
@@ -1,124 +1,13 @@
-// console.log("Hello World!!!") 
-// console.log('Hello World!!!');
+let usuario = "admin";
+let password = "1234";
+let inputContraseña = prompt("Ingrese su Contraseña");
 
-
-// prompt('Ingrese cualquier cosa')
-
-/* Variables */
-
-// var nombre = 'Jaime Zapata'
-// console.log(nombre)
-// var nombre = prompt('Ingrese el nombre de alguien: ')
-// console.log(nombre)
-
-/*  */
-
-// var numeroUno = parseInt(prompt('Ingrese un numero: '))
-// var numeroDos = parseInt(prompt('Ingrese un numero: '))
-// console.log(numeroUno + numeroDos)
-
-// console.log('2' + 2 + '2')
-// console.log((2 + 2) + '2')
-// console.log((2 + 2) - '2')
-// console.log('2' + '2' - '2')
-
-/* Tipos de datos */
-
-// var variable = 23
-// console.log(typeof (variable))
-// var variable = '23'
-// console.log(typeof (variable))
-// var variable = true
-// console.log(typeof (variable))
-// var variable
-// console.log(typeof (variable))
-// var variable = null
-// console.log(typeof (variable))
-
-
-/* Condicionales */
-/*if (5 > 5) {
-    console.log('Esto es un un condicional simple')
-} else if (5 < 5) {
-    console.log('Este es el condicional anidado')
+let inputUsuario = prompt("Ingrese su Usuario");
+if (usuario == inputUsuario) {
+  inputContraseña = prompt("Ingrese su contraseña");
+  if (password == inputContraseña) {
+    console.log("Bienvenido:" + inputUsuario);
+  } else console.log("Contraseña incorrecta");
 } else {
-    console.log('Esto es un condicional compuesto')
+  console.log("Usuario no existe en la base de datos ");
 }
-let opcion = prompt('Seleccione la opción: 1 , 2, 3, 4')
-switch (opcion) {
-    case '1':
-        console.log('Esta es la opción 1')
-        break
-    case 2:
-        console.log('Esta es la opción 2')
-        break
-    case 3:
-        console.log('Esta es la opción 3')
-        break
-    case 4:
-        console.log('Esta es la opción 4')
-        break
-    default:
-        console.log('No existe el caso')
-        break
-}
-
-/*let edad = 18
-let resultado = edad >= 18 ? true : false
-console.log(resultado)
-*/
-/*//var numero=prompt("Ingrese el numero de la tabla de multiplicar ")
-//for(let index = 0; index <= 10; index++){
-  //  console.log(numero +'*'+index+ '='+(numero * index))
-//}
-//var sumasalario = 0
-
-//for(var index=1; index <= 5; index++){
-//    var salario= Number(prompt ('Ingrese su salario:'))
-  //  sumasalario += salario
-    
-    
-}
-
-console.log('su salario'+(sumasalario/(index-1)))
-
-*/
-
-
-
-// var index = 1
-// var contador = 0
-// var sumasalario = 0
-// while (index == 1) {
-//     var salario = Number(prompt('ingrese su salario'))
-//     sumasalario += salario
-//     var opcion = Number(prompt('desea ingresar otro salario? \n 1= Si \n 2= No'))
-// contador++
-//     if (opcion == 2) {
-//         index = 2
-//     }
-
-// }
-// console.log('su salario' + (sumasalario / (contador - 1)))
-
-
-// var index = 1
-// var sumasalario = 0
-
-// do {
-//     var salario = Number(prompt('ingrese su salario'))
-//     sumasalario += salario
-//     var opcion = Number(prompt('desea ingresar otro salario? \n 1= Si \n 2= No'))
-//     index++
-
-// } while (index <= 5)
-
-// console.log('su salario' + (sumasalario / (contador - 1)))
-
-
-var nombre = 'jaime'
-let edad = 33
-if(edad >= 18 )
-    var salario=900000
-
-console.log(nombre + 'Tiene un salario de'+salario)
